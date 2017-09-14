45d63df87b23fd7e5d69ff837d456f64009b21a9
ale.llavaneres.11 <ale.llavaneres.11@gmail.com>
2017-09-14 13:24:27 +0200

    fa

---
+++ 1 Intro/Repas Vector i Deque.cpp
@@ -0,0 +1,27 @@
+/*
+Vector
+	[1|2|3|4|5|6|............|n]
+	estan en llocs consecutius de memoria
+	llista de tamany dinamic, implementada sense fer servir punters
+	podem accedri a qualsevol posicio de la llista amb []
+
+	OPERACIONS
+		[] -> accesso al elemento de la posicion...
+		front
+		back
+		push back
+		pop back
+		insert
+		erase
+		resize
+		size (tamany actual del vector)
+		capacity (size + espacio reservado de memoria)
+		max size
+
+Deque
+	[1][2][3][4][5][6]............[n]
+	en llocs de memoria no conescutius
+	implementat per punters
+	podem accedri a qualsevol posicio de la llista amb []
+
+*/
